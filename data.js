1a111d9a5f1836a8989ed4d635b800125bbab84e
rarehero-ref <sardorjorayev5600@gmail.com>
2025-08-09 16:20:17 +0500

    Create data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const kirishBallari = {
+    "2024": {
+        "Toshkent shahri": {
+            "Kompyuter ilmlari va dasturlash": { grant: 180.5, kontrakt: 155.2 },
+            "Tibbiyot": { grant: 185.1, kontrakt: 160.8 },
+            "Iqtisodiyot": { grant: 175.0, kontrakt: 145.5 }
+        },
+        "Samarqand viloyati": {
+            "Tarix": { grant: 165.3, kontrakt: 130.1 },
+            "Arxitektura": { grant: 170.8, kontrakt: 142.0 }
+        }
+    },
+    "2023": {
+        "Toshkent shahri": {
+            "Kompyuter ilmlari va dasturlash": { grant: 178.9, kontrakt: 152.1 },
+            "Tibbiyot": { grant: 183.5, kontrakt: 158.4 }
+        },
+        "Buxoro viloyati": {
+            "Kimyo texnologiyasi": { grant: 168.2, kontrakt: 135.7 },
+            "Filologiya": { grant: 160.0, kontrakt: 125.9 }
+        }
+    }
+};
